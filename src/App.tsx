f974b14de4aa5d8c62e2d2beb4a2b5e907cf9843
Lucas Gomes <lucaass1997rp@gmail.com>
2024-04-18 17:25:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from './pages/home'
+import { Cart } from './pages/cart'
+import { Layout } from "./pages/components/layout";
+import { ProductDetail } from "./pages/detail";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/cart",
+        element: <Cart/>
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetail/>
+      }
+    ]
+
+  }
+])
+
+export { router }
