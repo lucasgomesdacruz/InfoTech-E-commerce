ee3d06232298b68b085cfef31d31cd49e46ca059
Lucas Gomes <lucaass1997rp@gmail.com>
2024-04-18 23:36:12 -0300

    Adicionando componente de footer

---
+++ src/pages/components/footer/index.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return(
+        <footer className="w-full px-1 bg-slate-200 fixed bottom-0 left-0">
+                <div className="w-full max-w-7xl h-14 flex justify-center items-center px-5 mx-auto">
+                    <h1 className='font-bold'>© 2024 - Code by Lucas gomes.</h1>
+                </div>
+        </footer>            
+    )
+}
